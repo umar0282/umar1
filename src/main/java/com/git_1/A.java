dd774a91cf09dc6659a0425a5bfc8c97d8c664a2
Umar Farooq <umarf4517@gmail.com>
2024-11-18 17:04:38 +0530

    Defect -1237_Fixed Earlier Links were not working.

---
+++ src/main/java/com/git_1/A.java
@@ -0,0 +1,14 @@
+package com.git_1;
+
+import org.springframework.boot.origin.SystemEnvironmentOrigin;
+
+public class A {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git World");
+
+
+    }
+}
